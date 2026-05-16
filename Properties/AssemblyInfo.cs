0e60b3d8840680e829a76fe78184e6824863d791
SpookyKat22 <103320154+SpookyKat22@users.noreply.github.com>
2026-05-15 22:41:31 -0700

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General information about an assembly is controlled by
+// a set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OSRO Tools")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://github.com/torrq/4RTools-OSRO")]
+[assembly: AssemblyProduct("OSRO Tools")]
+[assembly: AssemblyCopyright("2025 by torrq | MIT License")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly invisible
+// to COM components. If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a7d78b00-6ce1-4b4d-90a3-1d2e93c7f68a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or default to Build and Revision Numbers
+// using the "*" as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.9.0")]
+[assembly: AssemblyFileVersion("1.0.9.0")]
+[assembly: NeutralResourcesLanguage("en")]
